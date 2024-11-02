71183641d7b0972695af0bea2f0946646f9959df
Isaac Lopez <isaac.lopez@upr.edu>
2024-11-02 06:08:56 -0400

    ide looking good bro

--- app/Main.hs
+++ app/Main.hs
@@ -3,24 +3,37 @@
 module Main where
 
 import Brick.AttrMap (AttrMap, AttrName, attrMap, attrName)
+import Brick.Focus (FocusRing, focusGetCurrent, focusNext, focusPrev, focusRing, withFocusRing)
 import Brick.Main (App (..), defaultMain, halt, showCursorNamed)
-import Brick.Types (BrickEvent (..), EventM, Widget)
-import Brick.Util (bg, on)
-import Brick.Widgets.Border (borderAttr, borderWithLabel)
-import Brick.Widgets.Border.Style (borderStyleFromChar)
-import Brick.Widgets.Center (hCenter)
+import Brick.Types (BrickEvent (..), CursorLocation, EventM, Widget)
+import Brick.Util (on)
+import Brick.Widgets.Border (border, borderWithLabel)
+import Brick.Widgets.Border.Style (borderStyleFromChar, unicodeBold)
+import Brick.Widgets.Center (hCenter, vCenter)
 import Brick.Widgets.Core (
     emptyWidget,
+    hBox,
+    hLimit,
     padAll,
     str,
+    vBox,
+    vLimit,
     withAttr,
     withBorderStyle,
+    (<+>),
+    (<=>),
  )
-import Brick.Widgets.Dialog (Dialog, renderDialog)
+import Brick.Widgets.Dialog (
+    Dialog,
+    buttonAttr,
+    buttonSelectedAttr,
+    dialog,
+    dialogSelection,
+    handleDialogEvent,
+    renderDialog,
+ )
 import Brick.Widgets.Edit (
     Editor,
-    editAttr,
-    editFocusedAttr,
     editor,
     getEditContents,
     handleEditorEvent,
@@ -28,54 +41,69 @@ import Brick.Widgets.Edit (
  )
 import Control.Monad (void)
 import Control.Monad.IO.Class
-import Data.Maybe (fromMaybe)
-import Graphics.Vty (brightWhite, defAttr, rgbColor)
+import Graphics.Vty (black, defAttr, white, yellow)
 import Graphics.Vty.Input.Events (Event (..), Key (..), Modifier (..))
 import Language (Program)
-import Lens.Micro ((^.))
-import Lens.Micro.Mtl (use, zoom)
+import Lens.Micro ((&), (^.), _Just)
+import Lens.Micro.Mtl (use, zoom, (%=), (.=), (?=))
 import Lens.Micro.TH (makeLenses)
 import Parser
 import Semantics
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
 
-data SaveOpt = Yes | No
-
 data NewFileOpt = Save | Cancel
 
 data Name
     = TextEditor
-    | TextEditorLines
-    | SavePrompt
-    | YesButton
-    | NoButton
-    | NewFilePrompt
     | SaveButton
     | CancelButton
+    | SaveTextEditor
+    | FocusFilePrompt
     deriving (Eq, Ord, Show)
 
 data St = St
     { _textEditor :: Editor String Name
     , _mfile :: Maybe FilePath
-    , _savePrompt :: Maybe (Dialog SaveOpt Name)
     , _newFilePrompt :: Maybe (Dialog NewFileOpt Name)
+    , _saveAs :: Maybe (Editor String Name)
+    , _newFileFocus :: Maybe (FocusRing Name)
     }
 makeLenses ''St
 
 initState :: Maybe FilePath -> String -> St
-initState f c = St (editor TextEditor Nothing c) f Nothing Nothing
+initState f c = St (editor TextEditor Nothing c) f Nothing Nothing Nothing
 
 draw :: St -> [Widget Name]
-draw st = drawSave st : [withBorderStyle (borderStyleFromChar ' ') $ withAttr borderLabelAttr $ borderWithLabel (str "Turbo Calculus") e]
+draw st = drawSave st : [vCenter $ padAll 1 $ maxWidth 200 $ vBox content]
   where
+    content = [textEd, instructions]
+    textEd = withBorderStyle (borderStyleFromChar ' ') $ withAttr borderLabelAttr $ borderWithLabel (str "Turbo Calculus") e
     e = renderEditor (str . unlines) True (st ^. textEditor)
+    maxWidth w = hCenter . hLimit w
+    instructions =
+        maxWidth 100 $
+            hBox
+                [ drawInstruction "^X" "Exit"
+                , drawInstruction "^S" "Save"
+                , drawInstruction "^C" "Compile"
+                ]
+
+drawInstruction :: String -> String -> Widget Name
+drawInstruction keys action =
+    withAttr keyAttr (str keys) <+> str " to " <+> str action & hCenter
 
 drawSave :: St -> Widget Name
-drawSave st =
-    case st ^. savePrompt of
-        Nothing -> emptyWidget
-        Just sd -> renderDialog sd $ hCenter $ padAll 1 $ str "Really want to save?"
+drawSave st = do
+    case (st ^. newFilePrompt, st ^. saveAs, st ^. newFileFocus) of
+        (Just sd, Just ed, Just f) -> renderDialog sd $ hCenter $ padAll 1 $ str "New file name" <=> drawSaveAs f ed
+        _ -> emptyWidget
+
+drawSaveAs :: FocusRing Name -> Editor String Name -> Widget Name
+drawSaveAs f ed = withBorderStyle unicodeBold $ border $ vLimit 1 $ label <+> saveAsEditor
+  where
+    saveAsEditor = withFocusRing f (renderEditor (str . unlines)) ed
+    label = str " Filter: "
 
 compile :: FilePath -> IO ()
 compile f = do
@@ -88,17 +116,79 @@ compile f = do
             print result
             exitSuccess
 
-saveFile :: EventM Name St ()
-saveFile = do
+saveFile :: FilePath -> EventM Name St ()
+saveFile file = do
     contents <- unlines . getEditContents <$> use textEditor
-    file <- use mfile
-    liftIO $ writeFile (fromMaybe "New.rc" file) contents
+    liftIO $ writeFile file contents
+
+createDialog :: Dialog NewFileOpt Name
+createDialog = dialog (Just $ str "Save new file") (Just (SaveButton, choices)) 50
+  where
+    choices =
+        [ ("Save", SaveButton, Save)
+        , ("Cancel", CancelButton, Cancel)
+        ]
+
+createSaveAs :: Editor String Name
+createSaveAs = editor SaveTextEditor Nothing ""
+
+createFocusRing :: FocusRing Name
+createFocusRing = focusRing [SaveTextEditor, SaveButton]
+
+saveNewPrompt :: EventM Name St ()
+saveNewPrompt = do
+    newFileFocus ?= createFocusRing
+    newFilePrompt ?= createDialog
+    saveAs ?= createSaveAs
 
 appEvent :: BrickEvent Name e -> EventM Name St ()
-appEvent (VtyEvent (EvKey (KChar 'x') [MCtrl])) = halt
-appEvent (VtyEvent (EvKey (KChar 's') [MCtrl])) = saveFile
-appEvent (VtyEvent (EvKey (KChar 'c') [MCtrl])) = halt
-appEvent ev = zoom textEditor $ handleEditorEvent ev
+appEvent ev@(VtyEvent e)
+    | isQuit e = quit
+    | otherwise = do
+        prompt <- use newFilePrompt
+        case prompt of
+            Just _ -> appEventDialog ev
+            Nothing -> appEventMain ev
+  where
+    isQuit (EvKey (KChar 'x') [MCtrl]) = True
+    isQuit (EvKey (KChar 'q') [MCtrl]) = True
+    isQuit (EvKey (KChar 'k') [MCtrl]) = True
+    isQuit (EvKey (KChar 'c') [MCtrl]) = True
+    isQuit _ = False
+appEvent _ = pure ()
+
+quit :: EventM Name St ()
+quit = halt
+
+appEventDialog :: BrickEvent Name e -> EventM Name St ()
+appEventDialog (VtyEvent (EvKey KEsc [])) = newFilePrompt .= Nothing
+appEventDialog (VtyEvent (EvKey KEnter [])) = do
+    filePrompt <- use newFilePrompt
+    case dialogSelection =<< filePrompt of
+        Just (CancelButton, Cancel) -> newFilePrompt .= Nothing
+        Just (SaveButton, Save) -> undefined
+        _ -> pure ()
+appEventDialog (VtyEvent (EvKey (KChar '\t') [])) = (newFileFocus . _Just) %= focusNext
+appEventDialog (VtyEvent (EvKey KBackTab [])) = (newFileFocus . _Just) %= focusPrev
+appEventDialog ev@(VtyEvent e) = do
+    f <- use newFileFocus
+    case f of
+        Nothing -> pure ()
+        Just fring ->
+            case focusGetCurrent fring of
+                Just SaveButton -> zoom (newFilePrompt . _Just) $ handleDialogEvent e
+                Just SaveTextEditor -> zoom (saveAs . _Just) $ handleEditorEvent ev
+                _ -> pure ()
+appEventDialog ev = zoom (saveAs . _Just) $ handleEditorEvent ev
+
+appEventMain :: BrickEvent Name e -> EventM Name St ()
+appEventMain (VtyEvent (EvKey (KChar 's') [MCtrl])) = do
+    f <- use mfile
+    maybe saveNewPrompt saveFile f
+appEventMain ev = zoom textEditor $ handleEditorEvent ev
+
+keyAttr :: AttrName
+keyAttr = attrName "key"
 
 borderLabelAttr :: AttrName
 borderLabelAttr = attrName "borderLabel"
@@ -107,17 +197,22 @@ appMap :: AttrMap
 appMap =
     attrMap
         defAttr
-        [ (editAttr, brightWhite `on` rgbColor (0 :: Integer) 0 255)
-        , (editFocusedAttr, brightWhite `on` rgbColor (0 :: Integer) 0 255)
-        , (borderAttr, bg $ rgbColor (0 :: Integer) 0 255)
-        , (borderLabelAttr, rgbColor (0 :: Integer) 0 255 `on` brightWhite)
+        [ (keyAttr, black `on` white)
+        , (borderLabelAttr, black `on` white)
+        , (buttonAttr, white `on` black)
+        , (buttonSelectedAttr, black `on` yellow)
         ]
 
+appCursor :: St -> [CursorLocation Name] -> Maybe (CursorLocation Name)
+appCursor st cursors =
+    case st ^. newFilePrompt of
+        Just _ -> showCursorNamed SaveTextEditor cursors
+        Nothing -> showCursorNamed TextEditor cursors
 app :: App St e Name
 app =
     App
         { appDraw = draw
-        , appChooseCursor = const $ showCursorNamed TextEditor
+        , appChooseCursor = appCursor
         , appHandleEvent = appEvent
         , appStartEvent = pure ()
         , appAttrMap = const appMap
